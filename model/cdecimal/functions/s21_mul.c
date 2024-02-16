24b45ed0e12a62a340899a5e684d653db0bf40c4
alexvnderftw <alex2561jun@gmail.com>
2024-02-17 08:28:18 +0900

    FIX: s21 decimal deleted.

--- model/cdecimal/functions/s21_mul.c
+++
@@ -1,8 +0,0 @@
-#include "big_decimal/big_decimal.h"
-#define SIGN_POS 127
-
-int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  big_decimal a = decimal_to_bd(value_1), b = decimal_to_bd(value_2), res;
-  bd_mul(a, b, &res);
-  return bd_to_decimal(res, result);
-}
--- model/cdecimal/functions/s21_sub.c
+++
@@ -1,8 +0,0 @@
-#include "./big_decimal/big_decimal.h"
-
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  big_decimal a = decimal_to_bd(value_1), b = decimal_to_bd(value_2), res;
-  b.sign = !b.sign;
-  bd_add(a, b, &res);
-  return bd_to_decimal(res, result);
-}
